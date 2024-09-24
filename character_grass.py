c47b7a2b46a218b3751010e22898b6ba277cf1e6
dageonlee@gameengineerning <leedageon764@gmail.com>
2024-09-24 09:43:22 +0900

    코딩의 첫 번째 나무를 보지 말고 숲을 본다

--- character_grass.py
+++ character_grass.py
@@ -2,6 +2,15 @@ from pico2d import *
 
 open_canvas()
 
-# fill here
+grass=load_image('grass.png')
+boy=load_image('character.png')
+
+#코딩에 있어서 첫번째로 생각해야 할 것. 나무를 보지 말고 숲을 봐야 한다.
+#사각형 운동과 원 운동 무한루프
+
+while(True): #사각형 운동과 원 운동
+    run_rectangle()
+    run_circle()
+    
 
 close_canvas()
