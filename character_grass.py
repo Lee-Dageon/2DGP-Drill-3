2b59ab4f0e5b119be34c0d69bf7060ac8583e497
dageonlee@gameengineerning <leedageon764@gmail.com>
2024-09-24 11:16:53 +0900

    삼각형 함수 완성, 무한루프 주석 제거

--- character_grass.py
+++ character_grass.py
@@ -35,14 +35,25 @@ def run_left():
     pass
 
 
+
 def run_left_tri():
-    print('left')
+    for y in range(100, 500, 10):
+        x = 100 + (y - 100) * (400 - 100) / (500 - 100)  # 기울기를 계산하여 x 좌표를 계산
+        draw_boy(x, y)
+        delay(0.01) 
     pass
+
 def run_right_tri():
-    print('right')
+    for x in range(400, 700, +10):
+        y = 100 + (x - 700) * (500 - 100) / (400 - 700)  # 기울기를 계산하여 y 좌표를 계산
+        draw_boy(x, y)
+        delay(0.01)
     pass
+
 def run_bottom_tri():
-    print('bottom')
+    for x in range(700, 100, -10):
+        draw_boy(x, 100)
+        delay(0.01)
     pass
 
 
@@ -57,7 +68,6 @@ def run_rectangle():
 
 #원 모양 함수 정의
 def run_circle():
-    print('circle')
 
     r=300
     cx=800 //2
@@ -77,10 +87,9 @@ def run_triangle():
 
 
 while(True): #사각형 운동과 원 운동
-    #run_circle()
-    #run_rectangle()
+    run_circle()
+    run_rectangle()
     run_triangle()
-    break
     
     
 
