35d4cbcf8183b8c3445cf1f1c42450624c9efec8
dageonlee@gameengineerning <leedageon764@gmail.com>
2024-09-24 10:51:23 +0900

    함수를 이용해 실행 가능한 간단한 코드를 만들고 주석을 처리해서 개발 시간 단축

--- character_grass.py
+++ character_grass.py
@@ -8,8 +8,28 @@ open_canvas()
 grass=load_image('grass.png')
 boy=load_image('character.png')
 
+def run_top():
+    print('TOP')
+    pass
+
+def run_right():
+    print('RIGHT')
+    pass
+
+def run_bottom():
+    print('BOTTOM')
+    pass
+
+def run_left():
+    print('LEFT')
+    pass
+
 def run_rectangle():
     print('rectangle')
+    run_top()
+    run_right()
+    run_bottom()
+    run_left()
     
     pass
 
@@ -31,7 +51,7 @@ def run_circle():
 
 
 while(True): #사각형 운동과 원 운동
-    run_circle()
+    #run_circle()
     run_rectangle()
     break
     
